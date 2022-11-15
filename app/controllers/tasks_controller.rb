b7e30d87175c3565034c4b49218372fb60149456
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-15 12:26:23 -0300

    show route ready

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,16 @@
 class TasksController < ApplicationController
+  before_action :get_task, only: [:show]
 
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_task
+    @task = Task.find(params[:id])
   end
 end
