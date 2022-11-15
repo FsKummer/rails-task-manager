0443626711ee1a69aa6bfb090d92af04af1dc540
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-15 12:41:44 -0300

    new task routes completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,20 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 
   def get_task
     @task = Task.find(params[:id])
